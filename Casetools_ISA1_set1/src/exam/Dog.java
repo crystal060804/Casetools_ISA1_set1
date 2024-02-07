b0cc1060fbf1fafa80203d5c51e109f97fd2ce26
Crystal Barreto <crystalbarreto008@gmail.com>
2024-02-07 10:28:17 +0530

    first commit

---
+++ Casetools_ISA1_set1/src/exam/Dog.java
@@ -0,0 +1,17 @@
+package exam;
+
+public class Dog {
+	private String name;
+	private String breed;
+	public Dog(String name, String breed)
+	{
+		this.name=name;
+		this.breed=breed;
+	}
+	
+	public String getBreed() 
+	{
+		return breed;
+	}
+
+}
